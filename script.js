e7c3e833862221039c59dbeb7d95493645224759
Caranjah <kelvincaranjah@gmail.com>
2025-03-30 19:17:37 +0300

    assignment4

---
+++ script.js
@@ -0,0 +1,21 @@
+// Mobile menu functionality
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const navLinksItems = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    // Toggle nav
+    navLinks.classList.toggle('active');
+    
+    // Burger animation
+    burger.classList.toggle('toggle');
+    
+    // Animate links
+    navLinksItems.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+});
